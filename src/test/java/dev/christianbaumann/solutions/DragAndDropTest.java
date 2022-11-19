6f9a8a35fa66ecc3cb36f373e86258911c873bda
chrissbaumann <mail@christianbaumann.dev>
2022-11-19 18:24:49 +0100

    Added solution dragAndDrop()

--- src/test/java/dev/christianbaumann/solutions/DragAndDropTest.java
+++ src/test/java/dev/christianbaumann/solutions/DragAndDropTest.java
@@ -3,6 +3,8 @@ package dev.christianbaumann.solutions;
 import dev.christianbaumann.Base;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DragAndDropTest extends Base {
 
     @Test
@@ -11,9 +13,9 @@ public class DragAndDropTest extends Base {
 
         page.navigate("https://the-internet.herokuapp.com/drag_and_drop");
 
-        // Drag A onto B
+        page.locator("#column-a").dragTo(page.locator("#column-b"));
 
-        //Verify
+        assertEquals("B", page.locator("//*[@id=\"column-a\"]/header").textContent());
 
     }
 }
