68188ec5af5e9e19aba8791c641d97d87a3dde08
chrissbaumann <mail@christianbaumann.dev>
2022-11-19 17:24:52 +0100

    Cleaning

--- src/test/java/dev/christianbaumann/demo/LinkTest.java
+++ src/test/java/dev/christianbaumann/demo/LinkTest.java
@@ -1,36 +1,12 @@
 package dev.christianbaumann.demo;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import dev.christianbaumann.Base;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ClickLink {
-
-    private Playwright playwright = Playwright.create();
-    private Browser browser;
-    private BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions().setHeadless(false);
-    private Browser.NewContextOptions contextOptions = new Browser.NewContextOptions().setViewportSize(960, 1032);
-    private BrowserContext context;
-    private Page page;
-
-    @BeforeEach
-    void setup() {
-        browser = playwright.chromium().launch(launchOptions);
-        context = browser.newContext(contextOptions);
-    }
-
-    @AfterEach
-    void teardown(){
-        browser.close();
-    }
+public class LinkTest extends Base {
 
     @Test
     void clickLinkViaText() {
